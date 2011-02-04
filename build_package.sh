32b5aacb561194a51861eeb9b1e4b707c467cc84
Fabrizio Furnari <fab.furnari@gmail.com>
2011-02-04 13:04:48 +0100

    Removed all sections that require sudo permission. Added fakeroot to replace

--- build_package.sh
+++ build_package.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 #
 # Use this script to auto build the archeos-manual package.
-# Usage: sudo ./build_package.sh
+# Usage: ./build_package.sh
 #
 
 # set -e
-if [ "$(id -u)" != "0" ]; then
-    echo "Not root, exiting..."
-    exit 1
-fi 
+
 echo -n "   * Looking the control's version..."
 VERSION=`grep "Version" "./archeos-manual/DEBIAN/control" | cut -f 2 -d ' '`
 echo "Done"
 echo -n "   * Copying the package into temp dir..."
 /bin/cp -r ./archeos-manual/ /tmp/archeos-menu_${VERSION}_all
-echo "Done"
-echo -n "   * Setting permissions..."
-sudo chown -R root:root /tmp/archeos-menu_${VERSION}_all 
 echo "Done"
 echo -n "   * Removing trailing slash (if present)..."
 pack_name=$(echo "/tmp/archeos-menu_${VERSION}_all" | sed -e "s/\/*$//")
@@ -28,7 +22,7 @@ cd ..
 echo "Done"
 echo "   * And finally let's buld the package!'"
 echo
-dpkg -b $pack_name $pack_name.deb
+fakeroot dpkg-deb --build $pack_name $pack_name.deb
 echo "Done"
 echo
 lintian $pack_name.deb
