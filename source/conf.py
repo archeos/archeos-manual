db542bac5043fee8d0c2943884107ddbd865bfd2
Fabrizio Furnari <fab.furnari@gmail.com>
2013-01-18 13:42:48 +0100

    Changed theme

--- source/conf.py
+++ source/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'doc'
-copyright = u'2013, Fabrizio'
+project = u'ArcheOS Documentation'
+copyright = u'2013, ArcheOS Documentation Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
