dceed2b57ff655b97933798ca0ec0e6c8d26088c
Fabrizio Furnari <fab.furnari@gmail.com>
2013-01-19 22:07:19 +0100

    Added support for manpages

--- source/conf.py
+++ source/conf.py
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'ArcheOS', u'ArcheOS Documentation',
+    ('index', 'archeos', u'ArcheOS Documentation',
      [u'ArcheOS Documentation Team'], 1)
 ]
 
