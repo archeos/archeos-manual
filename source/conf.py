29a6119406b78cafa8cf811999ee6c1dca4627c5
Fabrizio Furnari <fab.furnari@gmail.com>
2015-02-20 22:57:48 +0100

    removed unused conf

--- source/conf.py
+++ source/conf.py
@@ -294,4 +294,4 @@ epub_copyright = u'2013, ArcheOS Documentation Team'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+#intersphinx_mapping = {'http://docs.python.org/': None}
